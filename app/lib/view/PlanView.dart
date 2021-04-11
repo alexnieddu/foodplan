ba6897d55c4848f9a06ca607a0c7b334b6023d46
alex <almost@gmx.de>
2021-04-11 10:53:36 +0200

    Dummy image for work in progress in slots

--- app/lib/view/PlanView.dart
+++ app/lib/view/PlanView.dart
@@ -1,6 +1,12 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'package:foodplan/model/Recipe.dart';
 import 'package:foodplan/data/RecipeDatabase.dart';
+
+const List<String> wipImageUrl = [
+  "assets/workinprogress.jpg",
+  "assets/workinprogress1.jpg"
+];
 
 class PlanView extends StatefulWidget {
   PlanViewState createState() => PlanViewState();
@@ -9,55 +15,67 @@ class PlanView extends StatefulWidget {
 class PlanViewState extends State<PlanView> {
   @override
   Widget build(BuildContext context) {
+    Random rand = Random();
+    int randInt = rand.nextInt(wipImageUrl.length);
     return Scaffold(
-        body: FutureBuilder(
-          builder: (context, slots) {
-            if (!slots.hasData) {
-              return Center(child: CircularProgressIndicator());
-            } else {
-              return ListView.builder(
-                itemCount: slots.data.length,
-                itemBuilder: (context, i) {
-                  return ListTile(
-                      leading: Icon(Icons.calendar_today),
-                      title: Text(slots.data[i].name),
-                      subtitle: Text(slots.data[i].recipe),
-                      trailing: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          IconButton(
-                              onPressed: () {
-                                RecipeDatabase.db
-                                    .getRnd(slots.data[i].recipeId)
-                                    .then((rndRecipe) {
-                                  RecipeDatabase.db.updateSlot(
-                                      rndRecipe.first.id, slots.data[i].id);
-                                  setState(() {});
-                                });
-                              },
-                              icon: Icon(Icons.refresh)),
-                          /* IconButton(
-                              icon: Icon(Icons.more_vert), onPressed: null) */
-                        ],
-                      ));
-                },
-              );
-            }
-          },
-          future: RecipeDatabase.db.getAllSlots(),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            RecipeDatabase.db.getAllSlots().then((value) {
-              value.forEach((element) {
-                _reloadAllSlots(recipeID: 0, slotID: element.id);
-              });
-            });
-          },
-          child: Icon(Icons.refresh),
-        ));
+        body: Center(
+            child: Image(
+                image: AssetImage(wipImageUrl[randInt]),
+                width: MediaQuery.of(context).size.width * 0.5)));
   }
+
+  // class PlanViewState extends State<PlanView> {
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //       body: FutureBuilder(
+  //         builder: (context, slots) {
+  //           if (!slots.hasData) {
+  //             return Center(child: CircularProgressIndicator());
+  //           } else {
+  //             return ListView.builder(
+  //               itemCount: slots.data.length,
+  //               itemBuilder: (context, i) {
+  //                 return ListTile(
+  //                     leading: Icon(Icons.calendar_today),
+  //                     title: Text(slots.data[i].name),
+  //                     subtitle: Text(slots.data[i].recipe),
+  //                     trailing: Row(
+  //                       mainAxisSize: MainAxisSize.min,
+  //                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+  //                       children: <Widget>[
+  //                         IconButton(
+  //                             onPressed: () {
+  //                               RecipeDatabase.db
+  //                                   .getRnd(slots.data[i].recipeId)
+  //                                   .then((rndRecipe) {
+  //                                 RecipeDatabase.db.updateSlot(
+  //                                     rndRecipe.first.id, slots.data[i].id);
+  //                                 setState(() {});
+  //                               });
+  //                             },
+  //                             icon: Icon(Icons.refresh)),
+  //                         /* IconButton(
+  //                             icon: Icon(Icons.more_vert), onPressed: null) */
+  //                       ],
+  //                     ));
+  //               },
+  //             );
+  //           }
+  //         },
+  //         future: RecipeDatabase.db.getAllSlots(),
+  //       ),
+  //       floatingActionButton: FloatingActionButton(
+  //         onPressed: () {
+  //           RecipeDatabase.db.getAllSlots().then((value) {
+  //             value.forEach((element) {
+  //               _reloadAllSlots(recipeID: 0, slotID: element.id);
+  //             });
+  //           });
+  //         },
+  //         child: Icon(Icons.refresh),
+  //       ));
+  // }
 
   _reloadAllSlots({int recipeID, int slotID}) {
     RecipeDatabase.db.getRnd(recipeID).then((rndRecipe) {
