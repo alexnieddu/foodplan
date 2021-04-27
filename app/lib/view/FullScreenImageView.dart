c275dc3fe55131d87db767923f8d08135ce1d54c
alex <almost@gmx.de>
2021-04-27 10:39:49 +0200

    Photo view for zooming

--- app/lib/view/FullScreenImageView.dart
+++ app/lib/view/FullScreenImageView.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:photo_view/photo_view.dart';
 
 class FullScreenImageView extends StatelessWidget {
   final String src;
@@ -12,9 +13,11 @@ class FullScreenImageView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     if (isRemote) {
-      return Hero(tag: heroTag, child: Image.network(src));
+      return Hero(
+          tag: heroTag, child: PhotoView(imageProvider: NetworkImage(src)));
     } else {
-      return Hero(tag: heroTag, child: Image.file(File(src)));
+      return Hero(
+          tag: heroTag, child: PhotoView(imageProvider: FileImage(File(src))));
     }
   }
 }
